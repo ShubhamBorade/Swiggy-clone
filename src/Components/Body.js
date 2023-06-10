d7e37eb549bea94c48d45f87739b4f952c8881b2
Shubham <shubhamborade14899@gmail.com>
2023-06-10 19:11:09 +0530

    dummy data integrated

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import RestaurantCard from './RestaurantCard'
+import { RestaurantList } from '../Utils/Constants'
 
 const Body = () => {
     return (
         <div>
-           <div className='search'></div>
+{/* {  filter login with rating greater than 4} */}
+            <button className='m-3' onClick={()=>{
+                RestaurantList = RestaurantList.filter((res)=>res.data.avgRating < 4 )
+                console.log(RestaurantList);
+             } }>Top Rated Restaurant</button>
            <div className='restaurant-container flex flex-wrap mx-3'>
-            <RestaurantCard imgUrl="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ee5f8e06b300efc07c9fe3f4df40dfc4"/>
-            <RestaurantCard imgUrl="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ee5f8e06b300efc07c9fe3f4df40dfc4"/>
+             { 
+               RestaurantList.map((restaurant)=>(
+              <RestaurantCard key={restaurant.data.id}  resList={restaurant}/>
+              ))} 
            </div>
         </div>
     )
--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { CDN_URL } from '../Utils/Constants';
 
-const RestaurantCard = ({imgUrl}) => {
+const RestaurantCard = ({resList}) => {
+
+    const {name,cuisines,cloudinaryImageId,costForTwo,deliveryTime,avgRating} =resList.data;
     return (
         <div className='mt-8 '>
-            <div className='border-2 border-gray-100  w-52 shadow-lg ml-1 p-1 hover:cursor-pointer hover:shadow-xl mx-2 bg-gray-100'>
-                <img className='w-48 mx-auto mt-1 h-44' src={imgUrl} alt="resto-img" />
-                <h3 className='ml-1'>Meghana foods</h3>
-                <h4 className='ml-1'>Biryani, north indian</h4>
-                <h4 className='ml-1'>4.4 stars</h4>
-                <h4 className='ml-1'>36 minutes</h4>
+            <div className='border-2 border-gray-100  w-56 shadow-lg ml-1 p-1 hover:cursor-pointer hover:shadow-xl mx-4 bg-gray-100'>
+                <img className='w-48 mx-auto mt-1 h-44' src={CDN_URL+cloudinaryImageId} alt="resto-img" />
+                <h3 className='ml-1'>{name}</h3>
+                <h4 className='ml-1 flex-wrap'>{cuisines[0]}</h4>
+                <h4 className='ml-1'>{avgRating}</h4>
+                <h4 className='ml-1'>{costForTwo / 100}RS</h4>
+                <h4 className='ml-1'>{deliveryTime} minutes</h4>
             </div>
         </div>
     )
