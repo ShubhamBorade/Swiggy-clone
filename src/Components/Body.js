245f2cbd4e86ec67893694c8c13766b8a7099748
Shubham <shubhamborade14899@gmail.com>
2023-06-11 12:19:24 +0530

    filter feature added also fixed UI

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import RestaurantCard from './RestaurantCard'
 import { RestaurantList } from '../Utils/Constants'
 
 const Body = () => {
+
+  const [listofrestro, SetListOfRestro] = useState(RestaurantList)
+
     return (
         <div>
-{/* {  filter login with rating greater than 4} */}
-            <button className='m-3' onClick={()=>{
-                RestaurantList = RestaurantList.filter((res)=>res.data.avgRating < 4 )
-                console.log(RestaurantList);
-             } }>Top Rated Restaurant</button>
+            {/* {  filter logic with rating greater than 4} */}
+            <button className='m-3 mt-4 border-1 p-1 rounded-full px-4 bg-gray-200 border-gray-300 hover:bg-gray-400'  onClick={()=>{
+              const  filterRestro = listofrestro.filter((res)=> res.data.avgRating > 4 )
+                SetListOfRestro(filterRestro)
+            }}>Top Rated Restaurant</button>
            <div className='restaurant-container flex flex-wrap mx-3'>
              { 
-               RestaurantList.map((restaurant)=>(
+               listofrestro.map((restaurant)=>(
               <RestaurantCard key={restaurant.data.id}  resList={restaurant}/>
               ))} 
            </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,17 @@
+import logo from "../Utils/swiggy.svg"
 
 const Header = () =>{
     return (
-     <div className="flex justify-between bg-blue-100 shadow-lg">
+     <div className="flex justify-between bg-blue-50 shadow-lg">
       <div className="left">
-       <img className="w-36 h-16 " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxrzZDUk2YEJriFfZ-PKmPhZLZjtdxgSjpOA&usqp=CAU" alt="" />
+       <img className="w-32 h-14 my-1" src={logo} alt="logo" />
       </div>
       <div className="right">
-        <ul className="flex mt-5 mr-[100px]">
-            <li className="px-8 cursor-pointer hover:font-bold">Home</li>
-            <li className="px-8 cursor-pointer hover:font-bold">About</li>
-            <li className="px-8 cursor-pointer hover:font-bold">Contact Us</li>
-            <li className="px-8 cursor-pointer hover:font-bold">Cart</li>
+        <ul className="flex justify-between mt-3 mr-[120px]">
+            <li className="px-8 py-1 cursor-pointer hover:font-bold">Home</li>
+            <li className="px-8 py-1 cursor-pointer hover:font-bold">About</li>
+            <li className="px-8 py-1 cursor-pointer hover:font-bold">Contact Us</li>
+            <li className="px-8 py-1 cursor-pointer hover:font-bold">Cart</li>
         </ul>
 
       </div>
